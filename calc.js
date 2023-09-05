7749a7cae2ec875306e93450ebf3f285272b0096
mustafairshaidd <mustafa.irshaid.113@gmail.com>
2023-09-05 21:19:37 +0300

    the code is documented

--- calc.js
+++ calc.js
@@ -1,4 +1,5 @@
 export const calc = (...args) => {
+  // Priority for Operator Types
   const proiority = {
     "+": 1,
     "-": 1,
@@ -6,6 +7,7 @@ export const calc = (...args) => {
     "/": 2,
   };
 
+  // Operations functions
   const applyOperation = (left, operator, right) => {
     switch (operator) {
       case "+":
@@ -24,23 +26,35 @@ export const calc = (...args) => {
     }
   };
 
+  // Stack for operand
   const operandStack = [];
+
+  // Stack for operator
   const operatorStack = [];
 
+  // Loop the function paramaters
   args.forEach((token, index) => {
+    // Checks index : if it's even then its a number
     if (index % 2 == 0) {
+      // Checks if number is actually a number
       if (typeof token === "number") {
+        // If number is bigger than 1000
         if (token > 1000) {
-          if(operandStack.length> 0){
-             operatorStack.pop()
-          }  
-          else{
-            throw new Error("Invalid statement")
+          // If number is not the first argument then pop the operator from stack
+          if (operandStack.length > 0) {
+            operatorStack.pop();
+          }
+          // If number is first then its invalid statement
+          else {
+            throw new Error("Invalid statement");
           }
           return;
         }
+        // If its normal number then push it to operand
         operandStack.push(token);
-      } else {
+      }
+      // If its not normal then throw error
+      else {
         throw new Error("Invalid input type");
       }
     } else if (
@@ -51,19 +65,21 @@ export const calc = (...args) => {
         operatorStack.length > 0 &&
         proiority[operatorStack[operatorStack.length - 1]] >= proiority[token]
       ) {
+        //calculate the right and left operand with the right operator based on priority map with the value 2
         const rightOperand = operandStack.pop();
         const leftOperand = operandStack.pop();
         const op = operatorStack.pop();
-
         const result = applyOperation(leftOperand, op, rightOperand);
         operandStack.push(result);
       }
       operatorStack.push(token);
     } else {
+      // Else then its invalid operator -> does not exist in priority map
       throw new Error("Invalid operator");
     }
   });
 
+  //calculate the right and left operand with the right operator based on priority map with the value 1
   while (operatorStack.length > 0) {
     const rightOperand = operandStack.pop();
     const leftOperand = operandStack.pop();
@@ -72,15 +88,10 @@ export const calc = (...args) => {
     operandStack.push(result);
   }
 
+  // return the last value in stack
   if (operandStack.length === 1) {
     return operandStack[0];
   } else {
     throw new Error("Invalid expression");
   }
 };
-
-try {
-  calc(2, "+", 1001);
-} catch (e) {
-  console.log(e);
-}
