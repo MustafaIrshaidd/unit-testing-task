44e21feebbdcc4bc8ead67555541c9687f14af28
mustafairshaidd <mustafa-irshaid_2001@hotmail.com>
2023-09-05 15:30:04 +0300

    implemented calc function

--- calc.js
+++ calc.js
@@ -1,3 +1,25 @@
+const isNumber = (input) => {
+  return typeof input === "number";
+};
+
 export const calc = (num1, op, num2) => {
-  return a + b;
-}
+  if (!(isNumber(num1) && isNumber(num2))) {
+    throw new Error("Invalid input type");
+  }
+
+  switch (op) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    case '/':
+      if (num2 === 0) {
+        throw new Error("Cannot divide by 0");
+      }
+      return num1 / num2;
+    default:
+      throw new Error("Invalid Operator");
+  }
+};
